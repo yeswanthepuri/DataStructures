63d023ffc5a546fa9cb07cc5e2715faf5a53f154
Yeswanth <epuri.yeswanth1@gmail.com>
2024-08-23 13:26:58 +0530

    Linear Search

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Searching;
+
 Console.WriteLine("Hello, World!");
+
+LinearSearch<string> linearSearch = new LinearSearch<string>();
+string[] array = ["Delhi", "Hyderabad", "Vizag"];
+int result = linearSearch.Search(array, "Vizag");
+if (result > 0)
+{
+    Console.WriteLine(array[result]);
+}
--- Searching/LinearSearch.cs
+++ Searching/LinearSearch.cs
@@ -1,7 +1,42 @@
 ﻿namespace Searching
 {
-    public class Class1
+    public class LinearSearch<T> where T : IComparable<T>
     {
+        public int Search(T[] input, T itemToSearch)
+        {
+            if (input == null || input.Length == 0)
+            {
+                return -1;
+            }
 
+            Comparer<T> comparer = new Comparer<T>();
+
+            for (int index = 0; index < input.Length; index++)
+            {
+                if (comparer.Compare(input[index], itemToSearch) == 0)
+                {
+                    return index; // Item found, return its index
+                }
+            }
+
+            return -1; // Item not found
+        }
     }
+
+    public class Comparer<T> where T : IComparable<T>
+    {
+        public int Compare(T x, T y)
+        {
+            if (x == null)
+            {
+                return y == null ? 0 : -1;
+            }
+            if (y == null)
+            {
+                return 1;
+            }
+            return x.CompareTo(y);
+        }
+    }
+
 }
