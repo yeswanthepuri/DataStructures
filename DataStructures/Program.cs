1038559d5608ae352988904c19f026547d114477
Yeswanth <epuri.yeswanth1@gmail.com>
2024-08-25 13:53:59 +0530

    Binary Search.

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -2,11 +2,19 @@
 using Searching;
 
 Console.WriteLine("Hello, World!");
+#region LinearSearch
+//LinearSearch<string> linearSearch = new LinearSearch<string>();
+//string[] array = ["Delhi", "Hyderabad", "Vizag"];
+//int result = linearSearch.Search(array, "Vizag");
+//if (result > 0)
+//{
+//    Console.WriteLine(array[result]);
+//}
+#endregion
 
-LinearSearch<string> linearSearch = new LinearSearch<string>();
-string[] array = ["Delhi", "Hyderabad", "Vizag"];
-int result = linearSearch.Search(array, "Vizag");
-if (result > 0)
-{
-    Console.WriteLine(array[result]);
-}
+#region Binary Search
+int[] arraySorted = [10, 40, 45, 80, 100, 200, 420, 500, 520];
+BinarySearch binarySearch = new BinarySearch();
+//Console.WriteLine(binarySearch.SearchIterative(arraySorted, 520)); 
+Console.WriteLine(binarySearch.SearchRecursive(arraySorted, 200,0, arraySorted.Length -1)); 
+#endregion
---
+++ Searching/BinarySearch.cs
@@ -0,0 +1,54 @@
+﻿
+namespace Searching
+{
+    public class BinarySearch
+    {
+        public int SearchIterative(int[] array, int searchFor)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+            while (left <= right)
+            {
+                int median = (left + right) / 2;
+                if (array[median] == searchFor)
+                {
+                    return median;
+                }
+                else if (searchFor < array[median])
+                {
+                    right = median - 1;
+                }
+                else if (searchFor > array[median])
+                {
+                    left = median + 1;
+                }
+            }
+            return -1;
+        }
+
+        public int SearchRecursive(int[] array, int searchFor, int left, int right)
+        {
+            if (left > right)
+            {
+                return -1;
+            }
+            else
+            {
+                int median = (left + right) / 2;
+                if (array[median] == searchFor)
+                {
+                    return median;
+                }
+                else if (searchFor < array[median])
+                {
+                    return SearchRecursive(array, searchFor, left, median - 1);
+                }
+                else if (searchFor > array[median])
+                {
+                    return SearchRecursive(array, searchFor, median + 1, right);
+                }
+            }
+            return -1;
+        }
+    }
+}
