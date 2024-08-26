40ea5095e9b3228e8244806de4ebd819b44bd501
Yeswanth <epuri.yeswanth1@gmail.com>
2024-08-26 12:05:48 +0530

    Selection Soerting..

---
+++ Sorting/Print.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public static class Print
+    {
+        public static void WriteList(int[] list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Sorting/SelectionSort.cs
@@ -0,0 +1,34 @@
+﻿namespace Sorting
+{
+    public class SelectionSort
+    {
+        // Time Complixity = O(n power 2) Complixity based on loops
+
+        // Space Complixity = O(n) Based on variabled assigned or created
+        // Stability = Unstable 
+
+
+        //
+        public int[] Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length -1; i++) {
+                int position = i;
+
+                for (int j = i+1;j< array.Length;j++)
+                {
+                    if(array[j] < array[position])
+                    {
+                        position = j;
+                    }
+                }
+
+                int temp = array[i];
+                array[i] = array[position];
+                array[position] = temp;
+            }
+            return array;
+
+        }
+
+    }
+}
